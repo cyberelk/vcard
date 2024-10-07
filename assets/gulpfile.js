0ae69390be264dca1bf6cb06561784a41bb44cbe
Jari-Hermann Ernst <jari-hermann.ernst@t-online.de>
2024-10-07 16:40:28 +0200

    Add gulp functions for copying webfonts, compile sass, concat and minify js

--- assets/gulpfile.js
+++ assets/gulpfile.js
@@ -1,5 +1,7 @@
 const { src, dest } = require("gulp");
 const concat = require("gulp-concat");
+const sass = require('gulp-sass')(require('sass'));
+const minify = require('gulp-uglify');
 
 function concatFiles(cb) {
   src([
@@ -21,5 +23,33 @@ function concatFiles(cb) {
 
   cb();
 }
-
 exports.concat = concatFiles;
+
+function minifyJsFile(cb){
+  src("../public_html/js/scripts.js")
+    .pipe(minify())
+    .pipe(dest("../public_html/js"));
+
+  cb();
+}
+exports.minify = minifyJsFile;
+
+function copy(cb) {
+  src([
+    "./googlefonts/*.ttf",
+    "./googlefonts/*.woff2",
+    "./vendor/font-awesome/webfonts/*.ttf",
+    "./vendor/font-awesome/webfonts/*.woff2",
+  ], {encoding: false})
+      .pipe(dest('../public_html/fonts'));
+  cb();
+}
+exports.copy = copy;
+
+function compileSass() {
+  return src('./sass/stylesheet.scss', { sourcemaps: true })
+    .pipe(sass().on('error', sass.logError))
+    .pipe(dest('../public_html/css/', { sourcemaps: '.' }));
+};
+exports.sass = compileSass;
+
